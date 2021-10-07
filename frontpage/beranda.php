1ceae32968b8541b90fc8ba938c2571d64f9fe82
dian154 <dian.arlina@undiksha.ac.id>
2021-10-07 01:11:34 -0700

    Dian Arlina

---
+++ frontpage/beranda.php
@@ -0,0 +1,77 @@
+<div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+			<h1 class="my-4 text-3xl md:text-5xl text-black-100 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">AXIS-Y Indonesia Official </h1>
+			<p>Beauty, Cosmetic & Personal Care</p>
+			<p>Skincare Korea untuk Anda</p>
+			<p>☀️ Berbasis iklim panas dan lembab</p>
+			<p>🌱 Vegan dan cruelty-free</p>
+			<p>👩 Cocok untuk kulit sensitif</p>
+		</div>
+		
+		<!--Right Col-->
+		<div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
+            <img class="w-5/6 mx-auto lg:mr-0 slide-in-bottom" src="asssets/f1.jpg"> 
+		</div>
+
+        <div class="grid grid-cols-2 gap-2">
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/f2.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Green Vital Energy Complex Sheet Mask 💚</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">Produk terbaru dalam rangkaian perawatan kulit 611 kami ini mengandung 61% Mugwort dan diformulasikan dengan 15 ekstrak tumbuhan untuk melembabkan, menenangkan, dan menenangkan kulit 😌
+</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/f3.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">New Skin Resolution Gel Mask? 🙌
+</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">Diformulasikan khusus untuk menenangkan dan mencerahkan kulit, gel mask ini penuh dengan antioksidan untuk memperbaiki skin barrier 🌿
+
+Dengan bahan-bahan utama seperti niacinamide, heartleaf, madecassoside, calendula, allantoin, dan bahan-bahan lainnya untuk memulai hari dengan kulit yang sempurna ✨</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/f4.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Mugwort Pore Clarifying Wash Off Pack</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">Mugwort Pore Clarifying Wash Off Pack kami memiliki semua yang kamu butuhkan untuk rutinitas skincare di malam hari yang dibutuhkan kulit untuk membersihkan pori-pori, mencerahkan, dan melembabkan kulit 🌿⁠
+
+Dengan 61% ekstrak mugwort yang dapat menenangkan iritasi dan kemerahan pada kulit, membersihkan pori-pori, dan memperkuat skin barrier kamu ✨
+⁠</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/f5.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Complete No-Stress Physical Sunscreen, SPF 50+ PA++++</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">Complete No-Stress Physical Sunscreen, SPF 50+ PA++++ kami tidak hanya melindungi kulit dari sinar UV yang berbahaya, tetapi juga menenangkan dan memperbaiki kulit dengan…⁠
+⁠
+🌿 Mugwort - Menenangkan peradangan dan iritasi yang disebabkan oleh sinar yang merusak⁠
+🌿 2% Niacinamide - Mengurangi kemerahan, memudarkan hiperpigmentasi, dan⁠
+🌿 Squalane - Antioksidan untuk melawan radikal bebas⁠</p>
+					</div>
+					</div>
+			</div>
+		</div>
+
+		</div>
---
+++ frontpage/rekap.php
@@ -0,0 +1,39 @@
+<div class="bg-gradient-to-r from-purple-400 to-green-500 hover:from-pink-1000 hover:to-green-500 text-white font-bold focus:ring transform transition duration-300 ease-in-out shadow-lg rounded-xl shadow-md overflow-hidden md:max-w-full slide-in-bottom">
+        <div class="px-8 mb-4 text-center py-12">
+          <h2 class="px-4 py-3 border text-center font-bold">Pemesanan Produk</h2>
+            
+              <label class="text-white block">
+                <span class="grid grid-cols-4 gap-4 p-2 text-gray-700 text-center">Nama Lengkap</span>
+                <input type="text" class="grid grid-cols-4 gap-4 p-2 block w-full mt-1 block w-full rounded-md bg-black border-transparent  focus:ring-0" placeholder="masukkan nama anda"/>
+              </label>
+              <label class="text-black block">
+                <span class="grid grid-cols-4 gap-4 p-2 text-black-700">No HP</span>
+                <input type="number" class="grid grid-cols-4 gap-4 p-2 block w-full mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" placeholder="masukkan no HP"/>
+              </label>
+              <label class="text-white block">
+                <span class=" grid grid-cols-4 gap-4 p-2 text-black">Alamat</span>
+                <input type="text" class="grid grid-cols-4 gap-4 p-2 mt-1 block w-full rounded-md bg-black border-transparent focus:ring-0"placeholder="masukkan Alamat anda"/>
+               
+                <label class="text-black block">
+                <span class=" grid grid-cols-4 gap-4 p-2 text-black">Jumlah Pemesanan</span>
+                <input type="number" class="grid grid-cols-4 gap-4 p-2 mt-1 block w-full rounded-md bg-white border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"placeholder="pilih jumlah pesanan"/>
+             
+                <label class="text-white block">
+                <span class="grid grid-cols-4 gap-4 p-2 text-black">Produk</span>
+                <select class="grid grid-cols-4 gap-4 p-2 block w-full mt-1  rounded-md bg-black border-transparent focus:ring-0" placeholder="pilih produk">
+                  <option>Mask Gel</option>
+                  <option>Sunscreen</option>
+                  <option>Sheetmask</option>
+                </select>
+              </label>
+              <label class="block">
+                <span class="grid grid-cols-4 gap-4 p-2 text-black">Keterangan pada penjual</span>
+                <textarea
+                  class="mt-1 block w-full rounded-md bg-black border-transparent focus:border-gray-500 bg-white focus:ring-0" rows="3" class="text-center" placeholder="masukkan option lain"
+                ></textarea>
+              </label>
+              <button class=" grid grid-cols-4 gap-4 p-2  rounded-700 bg-gradient-to-r from-red-400 to-green-500 hover:from-blue-1000 hover:to-green-500 text-white font-bold focus:ring transform transition duration-300 ease-in-out shadow-lg block md:inline-block text-center">Pesan</button>
+            </div>
+            </div>
+        </div>
+</div>
